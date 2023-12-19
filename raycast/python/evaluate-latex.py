52cc59c399b28a8bc9d4bbb6f1ac1c22c2d58061
Jannis Metrikat <120120832+jmetrikat@users.noreply.github.com>
2023-12-19 19:24:54 +0100

    Add evaluate-latex.py script and antlr4-python3-runtime dependency

---
+++ raycast/python/evaluate-latex.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Evaluate LaTeX Expression
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon ../img/latex.png
+# @raycast.argument1 { "type": "text", "placeholder": "expression"}
+# @raycast.packageName jmetrikat
+
+# Documentation:
+# @raycast.description Evaluates a LaTeX expression using Python.
+# @raycast.author jmetrikat
+# @raycast.authorURL https://raycast.com/jmetrikat
+
+import pyperclip
+from sympy.parsing.latex import parse_latex
+import sys
+
+result = float(parse_latex(sys.argv[1]).evalf())
+
+if result.is_integer():
+    result = round(result)
+
+pyperclip.copy(result)
+print(result)
