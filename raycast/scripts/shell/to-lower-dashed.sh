8e26e2a0d7c63d50fa391b2436393d185f333a55
Jannis Metrikat <120120832+jmetrikat@users.noreply.github.com>
2024-04-30 21:51:47 +0200

    Add script to convert text to lower dashed format

---
+++ raycast/scripts/shell/to-lower-dashed.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title To lower dashed
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon ../img/cmd.png
+# @raycast.argument1 { "type": "text", "placeholder": "expression"}
+# @raycast.packageName jmetrikat
+
+# Documentation:
+# @raycast.description Convert to lower case and replace spaces with dashes.
+# @raycast.author jmetrikat
+# @raycast.authorURL https://github.com/jmetrikat
+
+result=$(echo $1 | tr '[:upper:]' '[:lower:]' | tr ' ' '-' | tr '_' '-' | sed 's/--/-/g')
+
+# print result and copy to clipboard
+echo "Copied to clipboard: " $result
+echo $result | pbcopy
