17b13cc87cea37700e521f486fe2d08f249885f2
Jannis Metrikat <120120832+jmetrikat@users.noreply.github.com>
2024-10-05 11:43:53 +0200

    feat: add homeserver connect

---
+++ raycast/scripts/shell/homeserver-connect.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Connect to Homeserver
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.needsConfirmation false
+# @raycast.icon 🔌
+# @raycast.packageName jmetrikat
+
+# Documentation:
+# @raycast.description Connect via SSH to Homeserver.
+# @raycast.author jmetrikat
+# @raycast.authorURL https://raycast.com/jmetrikat
+
+for i in {100..105}
+do
+  if ping -c 1 192.168.0.$i &> /dev/null
+  then
+    echo "Trying to connect to 192.168.0.$i"
+    if ssh -o BatchMode=yes -o ConnectTimeout=2 -p 69 192.168.0.$i exit &> /dev/null
+    then
+        echo "Connected to 192.168.0.$i"
+        echo "ssh -p 69 192.168.0.$i" | pbcopy
+        exit 0
+    fi
+  fi
+done
